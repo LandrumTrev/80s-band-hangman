60ad7dbd79eb926abe804608ba5528ce4e21cda5
ghostfountain <30249360+LandrumTrev@users.noreply.github.com>
2018-08-16 16:28:40 -0400

    variable primitives

--- assets/javascript/game-pseudo.js
+++ assets/javascript/game-pseudo.js
@@ -1,63 +1,121 @@
 // Pseudocode for Word Guess Game aka Hangman, 80s Style
 
 
-// (ON PAGE LOAD:
+// ON PAGE LOAD--put all these variables in an OBJECT
 
 
 let bandsArray = [
 
-  {name: ["A", "-", "H", "A"],
-   photo: "<img src:../images/aha.jpg>"},
-   mp3: "<img src:../music/aha.mp3>"},
-   song: "The Sun Always Shines On TV by a-ha"},
+    {
+        name: ["A", "-", "H", "A"],
+        photo: "<img src:../images/aha.jpg>",
+        mp3: "<img src:../music/aha.mp3>",
+        song: "The Sun Always Shines On TV by a-ha"
+    },
 
-  {name: ["T", "H", "E", " ", "P", "O", "L", "I", "C", "E"],
-   photo: "<img src:../images/thepolice.jpg>"},
-   mp3: "<img src:../music/thepolice.mp3>"},
-   song: "Spirits In The Material World by The Police"},
+    {
+        name: ["T", "H", "E", " ", "P", "O", "L", "I", "C", "E"],
+        photo: "<img src:../images/thepolice.jpg>",
+        mp3: "<img src:../music/thepolice.mp3>",
+        song: "Spirits In The Material World by The Police"
+    },
 
-  {name: ["R", "U", "S", "H"],
-   photo: "<img src:../images/rush.jpg>"},
-   mp3: "<img src:../music/rush.mp3>"},
-   song: "Red Barchetta by Rush"},
+    {
+        name: ["R", "U", "S", "H"],
+        photo: "<img src:../images/rush.jpg>",
+        mp3: "<img src:../music/rush.mp3>",
+        song: "Red Barchetta by Rush"
+    },
 
-  {name: ["S", "T", "E", "V", "I", "E", " ", "N", "I", "C", "K", "S"],
-   photo: "<img src:../images/stevienicks.jpg>"},
-   mp3: "<img src:../music/stevienicks.mp3>"},
-   song: "Blue Lamp by Stevie Nicks"},
+    {
+        name: ["S", "T", "E", "V", "I", "E", " ", "N", "I", "C", "K", "S"],
+        photo: "<img src:../images/stevienicks.jpg>",
+        mp3: "<img src:../music/stevienicks.mp3>",
+        song: "Blue Lamp by Stevie Nicks"
+    },
 
-  {name: ["B", "O", "B", " ", "M", "A", "R", "L", "E", "Y"],
-   photo: "<img src:../images/bobmarley.jpg>"},
-   mp3: "<img src:../music/bobmarley.mp3>"},
-   song: "Small Axe by Bob Marley and the Wailers"},
+    {
+        name: ["B", "O", "B", " ", "M", "A", "R", "L", "E", "Y"],
+        photo: "<img src:../images/bobmarley.jpg>",
+        mp3: "<img src:../music/bobmarley.mp3>",
+        song: "Small Axe by Bob Marley and the Wailers"
+    },
 
-  {name: ["T", "H", "E", " ", "C", "L", "A", "S", "H"],
-   photo: "<img src:../images/theclash.jpg>"},
-   mp3: "<img src:../music/theclash.mp3>"},
-   song: "Lost In The Supermarket by The Clash"},
+    {
+        name: ["T", "H", "E", " ", "C", "L", "A", "S", "H"],
+        photo: "<img src:../images/theclash.jpg>",
+        mp3: "<img src:../music/theclash.mp3>",
+        song: "Lost In The Supermarket by The Clash"
+    },
 
-  {name: ["M", "A", "R", "V", "I", "N", " ", "G", "A", "Y", "E"],
-   photo: "<img src:../images/marvingaye.jpg>"},
-   mp3: "<img src:../music/marvingaye.mp3>"},
-   song: "Sexual Healing by Marvin Gaye"},
+    {
+        name: ["M", "A", "R", "V", "I", "N", " ", "G", "A", "Y", "E"],
+        photo: "<img src:../images/marvingaye.jpg>",
+        mp3: "<img src:../music/marvingaye.mp3>",
+        song: "Sexual Healing by Marvin Gaye"
+    },
 
-  {name: ["B", "I", "L", "L", "Y", " ", "I", "D", "O", "L"],
-   photo: "<img src:../images/billyidol.jpg>"},
-   mp3: "<img src:../music/billyidol.mp3>"},
-   song: "Eyes Without A Face by Billy Idol"},
+    {
+        name: ["B", "I", "L", "L", "Y", " ", "I", "D", "O", "L"],
+        photo: "<img src:../images/billyidol.jpg>",
+        mp3: "<img src:../music/billyidol.mp3>",
+        song: "Eyes Without A Face by Billy Idol"
+    },
 
-  {name: ["D", "U", "R", "A", "N", " ", "D", "U", "R", "A", "N"],
-   photo: "<img src:../images/duranduran.jpg>"},
-   mp3: "<img src:../music/duranduran.mp3>"},
-   song: "The Reflex by Duran Duran"},
+    {
+        name: ["D", "U", "R", "A", "N", " ", "D", "U", "R", "A", "N"],
+        photo: "<img src:../images/duranduran.jpg>",
+        mp3: "<img src:../music/duranduran.mp3>",
+        song: "The Reflex by Duran Duran"
+    },
 
-  {name: ["T", "H", "E", " ", "C", "A", "R", "S"],
-   photo: "<img src:../images/thecars.jpg>"},
-   mp3: "<img src:../music/thecars.mp3>"},
-   song: "Drive by The Cars"},
+    {
+        name: ["T", "H", "E", " ", "C", "A", "R", "S"],
+        photo: "<img src:../images/thecars.jpg>",
+        mp3: "<img src:../music/thecars.mp3>",
+        song: "Drive by The Cars"
+    },
 
 ];
 
+let bandChoice = 9;
+// choose a random array element from bandsArray[i]
+// use to keep track of name, photo, mp3, and song name of current word
+
+let wordChoice = ["T", "H", "E", " ", "C", "A", "R", "S"];
+// an Array of individual CAPITAL letters, 
+// from the .name of the current bandsArray[bandChoice] object
+// an Array used to compare right/wrong answers against for insertion into wordDisplay
+
+let wordDisplay = ["_", "_", "_", "_", "_", "_", "_", "_"];
+// an Array; for each letter element in the wordChoice array above,
+// create an element in this array that is "_"
+// used to display the current status of the guessed word in HTML
+
+let guessesRemaining = 12;
+// used to keep track of how many guesses the user has left
+
+let userGuess = "a";
+// initialize but leave empty.
+// used to track the value of each key pressed by user as a guess
+// value is set in the DOCUMENT.KEYUP() function as event.key.toUpperCase()
+
+let lettersGuessed = ["D", "H", "K", "Q"];;
+// an Array, empty on page load and at reset()
+// used to keep track of each new (not previously pressed) key value in userGuess
+
+let bandPhoto = "<img src:../images/thecars.jpg>";
+// get photo from the .photo of the current bandsArray[bandChoice] object
+
+let bandSong = "<img src:../music/thecars.mp3>";
+// get photo from the .photo of the current bandsArray[bandChoice] object
+
+let wins = 0;
+// start at 0 (or unset? as in the example)
+
+
+// END OF OBJECT PRIMITIVE, OBJECT, AND ARRAY VARIABLES
+// START OBJECT FUNCTION PROPERTIES (METHODS?)
 
 
 
@@ -69,36 +127,11 @@ let bandsArray = [
 
 
 
+// END OF OBJECT CONTAINING ALL VARIABLES
 
+// START GAME PLAY TRIGGER FUNCTION
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.onkeyup = function (event) {
+    userGuess = event.key.toUpperCase();
+    play();
+}
